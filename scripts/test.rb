fde30d2f4982f8de9a5f1ff95ca4d865075fe346
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2012-09-26 10:37:53 +0200

    initial commit

---
+++ scripts/test.rb
@@ -0,0 +1,28 @@
+require 'orocos'
+include Orocos
+
+if !ARGV[0]
+    STDERR.puts "usage: test.rb <device name>"
+    exit 1
+end
+
+ENV['PKG_CONFIG_PATH'] = "#{File.expand_path("..", File.dirname(__FILE__))}/build:#{ENV['PKG_CONFIG_PATH']}"
+
+Orocos.initialize
+
+Orocos::Process.run 'stim300_driver_test' do |p|
+    driver = p.task 'stim300'
+    Orocos.log_all_ports
+
+    driver.port = ARGV[0]
+    driver.configure
+    driver.start
+
+#     reader = driver.orientation_samples.reader(:type => :buffer, :size => 100)
+    loop do        
+#  	while sample = reader.read_new
+#            print("#{sample.time.to_f} #{sample.orientation.x} #{sample.orientation.y} #{sample.orientation.z} #{sample.orientation.w}\r\n")
+#  	end
+	sleep 0.01
+    end
+end
---
+++ tasks/Task.cpp
@@ -0,0 +1,128 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "Task.hpp"
+
+using namespace stim300;
+
+Task::Task(std::string const& name)
+    : TaskBase(name)
+{
+}
+
+Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
+    : TaskBase(name, engine)
+{
+}
+
+Task::~Task()
+{
+    delete timestamp_estimator;
+}
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See Task.hpp for more detailed
+// documentation about them.
+
+bool Task::configureHook()
+{
+    
+    std::string acc_acc ("ACCELERATION");
+    std::string acc_vel ("INCREMENTAL_VELOCITY");
+     
+    if (! TaskBase::configureHook())
+        return false;
+    
+    timestamp_estimator = new aggregator::TimestampEstimator(
+	base::Time::fromSeconds(20),
+	base::Time::fromSeconds(1.0 / stim300::STIM300Driver::DEFAULT_SAMPLING_FREQUENCY),
+	base::Time::fromSeconds(0),
+	INT_MAX);
+    
+    /** Set the baudrate to the value in the rock property *
+     * Default is 921600 **/
+     stim300_driver.setBaudrate(_baudrate.value());
+     
+     /** Open the port **/
+     if (!stim300_driver.open(_port.value()))
+     {
+	std::cerr << "Error opening device '" << _port.value() << "'" << std::endl;
+        return false;
+     }
+     
+     /** Set the output format for the accelerometers **/
+     if (acc_acc.compare(_acc_output.value()) == 0)
+     {
+	 /** NOTHING BECAUSE THE DEFAULT VALUE IN THE FLASH MEMORY IS ACCELERATION **/
+	 /** IF THE FLASH MEMORY IN THE STIM300 IS CHANGE TO ANOTHER DEFAULT VALUE SOMETHING NEED TO BE WRITEN HERE **/
+     }
+     else if (acc_vel.compare(_acc_output.value()) == 0)
+     {
+	 /** CHANGE THE OUTPUT TO INCREMENTAL_VELOCITY **/
+	 
+	 /** Change the configuration value **/
+	 stim300_driver.setAcctoIncrementalVelocity();
+	 
+     }
+     
+     
+    
+    return true;
+}
+bool Task::startHook()
+{
+    if (! TaskBase::startHook())
+        return false;
+    return true;
+}
+void Task::updateHook()
+{
+    
+    stim300_driver.getInfo();
+    stim300_driver.processPacket();
+    
+    if (stim300_driver.getStatus())
+    {
+// 	base::Time recvts = base::Time::now();
+// 
+// 	int packet_counter = m_driver->getPacketCounter();
+// 
+// 	base::Time ts = timestamp_estimator->update(recvts,packet_counter);
+//         timeout_counter = 0;
+	
+	base::Time ts = base::Time::now();
+	
+	base::samples::IMUSensors sensors;
+
+	sensors.time = ts;
+	sensors.acc = stim300_driver.getAccData();
+	sensors.gyro = stim300_driver.getGyroData();
+	
+	_calibrated_sensors.write( sensors );
+    }
+    
+    TaskBase::updateHook();
+}
+void Task::errorHook()
+{
+    TaskBase::errorHook();
+}
+void Task::stopHook()
+{
+    TaskBase::stopHook();
+    
+    stim300_driver.close();
+    
+    timestamp_estimator->reset();
+}
+void Task::cleanupHook()
+{
+    TaskBase::cleanupHook();
+    
+    stim300_driver.close();
+    
+    delete timestamp_estimator;
+    timestamp_estimator = NULL;
+}
+
